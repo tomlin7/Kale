c27a68d38981831dfef84f956f257401c934abdb
billyeatcookies <billydevbusiness@gmail.com>
2021-09-12 15:43:35 -0500

    Implement a keyword mapper

---
+++ src/Kale/codeanalysis/Syntax/tokenmap.py
@@ -0,0 +1,20 @@
+from codeanalysis.Syntax.tokenkind import TokenKind
+
+keywords = {
+    TokenKind.CharKeyword: 'char',
+    TokenKind.ElseKeyword: 'else',
+    TokenKind.FloatKeyword: 'float',
+    TokenKind.ForKeyword: 'for',
+    TokenKind.ForeachKeyword: 'foreach',
+    TokenKind.GotoKeyword: 'goto',
+    TokenKind.IfKeyword: 'if',
+    TokenKind.InKeyword: 'in',
+    TokenKind.InputKeyword: 'input',
+    TokenKind.IntKeyword: 'int',
+    TokenKind.LabelKeyword: 'label',
+    TokenKind.LetKeyword: 'let',
+    TokenKind.PrintKeyword: 'print',
+    TokenKind.StringToken: 'string',
+    TokenKind.VarKeyword: 'var',
+    TokenKind.WhileKeyword: 'while'
+}
